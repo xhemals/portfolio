9b87c04ab17c9539dd850467d827bbe0ebbb200a
Jamie Speller <j@miee.uk>
2024-03-21 02:20:56 +0000

    first commit

---
+++ src/pages/tfl-journey-planner.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import App from "@submodule/TFL-Journey-Planner/src/app/App";
+
+const TflJourneyPlanner = () => {
+	return <App />;
+};
+
+export default TflJourneyPlanner;
