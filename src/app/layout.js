ce1fae08880b1988051ebbada8af11d8ec5fd2dd
Jamie Speller <j@miee.uk>
2024-03-22 02:58:54 +0000

    Add async attribute to fontawesome script tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -32,6 +32,7 @@ export default function RootLayout({ children }) {
 					<script
 						src="https://kit.fontawesome.com/c4b491e14f.js"
 						crossOrigin="anonymous"
+						async
 					></script>
 				</body>
 				<Analytics />
